aef0993a0e6c0339f4ffb707211edfa6da351223
B3t4 <49000749+TuanB3T4@users.noreply.github.com>
2019-06-10 05:33:32 +0700

    Update Deface.sh

--- Deface.sh
+++ Deface.sh
@@ -19,12 +19,24 @@ echo   "[=] Team     : Anteiku Nigh Raid        [=] " |lolcat
 echo   "[=] Creadby  : Tuan B3T4                [=] " |lolcat
 echo   "[=] Contact  : 085694011450             [=] " |lolcat
 echo   "[=][===================================][=]" | lolcat
-echo $red"VERSION=1"
+echo $red"VERSION=1.2"
 echo $red"Nb:script deface nya agan taruh diluar folder di penyimpanan internal ya gan"
 #########################################################
 
 
-
+echo $white"---------------------------------------"
+echo $purple"                 LIST"
+echo $white"---------------------------------------"
+echo $red"[1].DEFACE"
+echo $red"[2].DEFACE BRUTAL"
+echo $red"[3].WEB VULN"
+echo $red"[4].EXIT"
+echo $white"---------------------------------------"
+echo    "╭─[=]B3T4[=]" Masukan Script anda |lolcat
+read -p "╰─#" bro
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
 echo
 echo    "╭─[=]B3T4[=]" Masukan Target anda |lolcat
 read -p "╰─#" target
@@ -38,23 +50,126 @@ echo $green"Hasil deface =>$target/$script"
 echo
 echo $blue"Terimakasih Telah Menggunakan tools ini"
 echo
-echo $white"---------------------------------------"
-echo $purple"                 LIST"
-echo $white"---------------------------------------"
-echo $red"[1].Deface"
-echo $red"[2].exit"
-echo $white"---------------------------------------"
-echo    "╭─[=]B3T4[=]" Masukan Script anda |lolcat
-read -p "╰─#" bro
-echo
-if [ $bro = 1 ] || [ $bro = 1 ]
-then
-clear
-sh Deface.sh
+echo    "╭─[=]B3T4[=]" Kembali? |lolcat
+read -p "╰─#" enter
+sh deface.sh
 fi
 
 echo
-if [ $bro = 2 ] || [ $bro = 2 ]
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+echo
+echo $cyan"http://bitsandpieces.co.za"
+echo $cyan"http://cblandscapes.co.za"
+echo $cyan"http://windmillsandporcupines.co.za"
+echo $cyan"http://scnc.co.za"
+echo $cyan"http://contsol.co.za"
+echo $cyan"http://consultec.co.za"
+echo $cyan"http://colourfactory.co.za"
+echo $cyan"http://ayk.co.za"
+echo $cyan"http://daretogowildafrica.co.za"
+echo $cyan"http://floralandfresh.co.za"
+echo $cyan"http://foodconsult.co.za"
+echo $cyan"http://itsengineering.co.za"
+echo $cyan"http://hybriddevelopments.co.za"
+echo $cyan"http://holotropicbreathwork.co.za"
+echo $cyan"http://hanover-cs.co.za"
+echo $cyan"http://handj.co.za"
+echo $cyan"http://lwrm.co.za"
+echo $cyan"http://lubbestrust.co.za"
+echo $cyan"http://lpenterprises.co.za"
+echo $cyan"http://kwalitycommodities.co.za"
+echo $cyan"http://mestern.co.za"
+echo $cyan"http://megro.co.za"
+echo $cyan"http://bitpc.co.za"
+echo $cyan"http://butterflowers.co.za"
+echo $cyan"http://beeit.co.za"
+echo $cyan"http://esfml.com"
+echo $cyan"http://bee-it.co.za"
+echo $cyan"http://mc3qs.com"
+echo $cyan"http://marjoriebresler.com"
+echo $cyan"http://naturaleyeimages.com"
+echo $cyan"http://my-resume.biz"
+echo $cyan"http://mpark.co.za"
+echo $cyan"http://montdestin.com"
+echo $cyan"http://africa-one.co.za"
+echo $cyan"http://ads247.co.za"
+echo $cyan"http://accelerate-kids.co.za"
+echo $cyan"http://8x8.co.za"
+echo $cyan"http://aaglcapetown2013.org.za"
+echo $cyan"http://allpaint.co.za"
+echo $cyan"http://andrewswebcorner.co.za"
+echo $cyan"http://applebyguesthouse.com"
+echo $cyan"http://atlanticphysio.co.za"
+echo $cyan"http://page31.co.za"
+echo $cyan"http://vaigaidigitals.com"
+echo $cyan"http://systemshouse.co.za"
+echo $cyan"http://www.studentsonline.co.za"
+echo $cyan"http://lucaniphysio.co.za"
+echo $cyan"http://www.onboardpublications.co.za"
+echo $cyan"http://www.oilandclay.co.za"
+echo $cyan"http://www.aquafitness.za.net"
+echo $cyan"http://www.helderbergit.co.za"
+echo $cyan"http://www.pipelinetape.co.za"
+echo $cyan"http://www.drcrsaexpress.co.za"
+echo $cyan"http://officeelf.co.za"
+echo $cyan"http://troop-1539.org"
+echo $cyan"http://happy.mikikk.co.jp"
+echo $cyan"http://www.alfunctiondesigns.co.za"
+echo $cyan"http://mix007.co.za"
+echo $cyan"http://spyroom.co.za"
+echo $cyan"http://sportspod.co.za"
+echo $cyan"http://southernafricablog.com"
+echo $cyan"http://stand66.com"
+echo $cyan"http://tapifare.com"
+echo $cyan"http://tankertech.co.za"
+echo $cyan"http://tcnig.co.za"
+echo $cyan"http://tcmgroup.co.za"
+echo $cyan"http://tressor.co.za"
+echo $cyan"http://trafficam.co.za"
+echo $cyan"http://tuthukani.co.za"
+echo $cyan"http://nyarhi.co.za"
+echo $cyan"http://digishop.co.za"
+echo $cyan"http://elinemsa.co.za"
+echo $cyan"http://disucctv.co.za"
+echo $cyan"http://www.cai.co.za"
+echo $cyan"http://tirupurjobs.net"
+echo $cyan"http://inlislitev3.perpusnas.go.id"
+echo $cyan"http://clcooling.co.za"
+echo $cyan"http://sublimeconfectionery.co.za"
+echo $cyan"http://gogreenplumbing.co.za"
+echo $cyan"http://golfbuzz.co.za"
+echo $cyan"http://grupo-cma.com"
+echo $cyan"http://inkspot.co.za"
+echo $cyan"http://kgarebana.co.za"
+echo $cyan"http://ittrader.co.za"
+echo $cyan"http://hellomobile.mobi"
+echo $cyan"http://homebuzz.co.za"
+echo $cyan"http://gordonhoward.co.za"
+echo $cyan"http://democonstruction.co.za"
+echo $cyan"http://chasingfantasia.co.za"
+echo $cyan"http://litehouse.co.za"
+echo $cyan"http://ldata.co.za"
+echo $cyan"http://lunique.co.za"
+echo $cyan"http://chillibitez.co.za"
+echo $cyan"http://cmweb.co.za"
+echo $cyan"http://essentialtours.com"
+echo $cyan"http://entitledvip.com"
+echo $cyan"http://frankadesign.com"
+echo $cyan"http://forget-me-not.co.za"
+echo $cyan"http://findpersonnel.co.za"
+echo $cyan"http://everythingdating.co.za"
+echo $purple"Pilih yang masih perawan"
+echo    "╭─[=]B3T4[=]" Sudah Nemu yang masih perawan? |lolcat
+read -p "╰─#" ya
+clear
+echo    "╭─[=]B3T4[=]" yuk kita bawa pulang |lolcat
+read -p "╰─#" enter
+sh deface.sh
+fi
+
+echo
+if [ $bro = 4 ] || [ $bro = 4 ]
 then
 echo   "[=][===================================][=]" | lolcat
 echo $blue"Yakin mau keluar?"
@@ -66,8 +181,7 @@ echo $red"Terimakasih Telah Menggunakan tools ini, Bila ada kekurangan dengan to
 echo
 echo
 echo
-echo
-echo
+echo    "╭─[=]B3T4[=]" Tekan Enter Untuk Keluar | lolcat
+read -p "╰─#" enter
 clear
 exit
-fi
